0e33fff6374a940d387c9e8a3714221962174292
Daniel Ortiz <daniel.ortizvega@mercadolibre.com>
2022-09-16 20:43:53 -0500

    mi primer servicio rest

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI, HTTPException
 
 app = FastAPI()
 
 
+lista_sentimientos = []
+
 #HOLA LAUUUU
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": lista_sentimientos}
+
+@app.post("/")
+async def add(request: Request):
+    persons = await request.json()
+    for emocion in lista_sentimientos:
+        if persons.get('feeling')==emocion.get('feeling') and persons.get('name')==emocion.get('name'):
+            raise HTTPException(status_code=400, detail="Feeling for that person already exists")
+
+    lista_sentimientos.append(await request.json())
+
+    return lista_sentimientos
+
+@app.put("/")
+async def update(request: Request):
+    persona = await request.json()
+    for emocion in lista_sentimientos:
+        if persona.get("name")== emocion.get("name"):
+            emocion["feeling"]=persona.get("feeling")
+    return lista_sentimientos
 
