3c946b1fadf3535ccc1e586c65ba28ffe57ce4e5
lauraromero <aleja-02061@hotmail.com>
2022-10-20 20:26:29 -0500

    Iniciando base de datos

---
+++ database_sql/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+
+from database_sql.connection import Base
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicle"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(45), unique=True, index=True)
+    brand = Column(String(45))
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(45))
+    last_name = Column(String(45))
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 from fastapi import Request, FastAPI, HTTPException
-
+from controllers import vehicle
+from database_sql.connection import Base, engine
+from settings import Settings
 from models.feeling import Moto
 
 app = FastAPI()
 
+settings= Settings()
+
 lista_moto: [Moto] = []
+
+Base.metadata.create_all(bind=engine)
 
 
 # HOLA LAUUUU
 @app.get("/")
 async def rooto():
-    return {"message": lista_moto}
+    return {"message": settings.mysql_uri}
 
-@app.get("/no_abs")
-async def noabs():
-    result = map(lambda item: {
-        "nombre": item.name
-    }, lista_moto)
-    print(result)
-    return list(result)
-
-@app.get("/{name}")
-async def root(name):
-    result = filter(lambda item: item.name == name, lista_moto)
-    print(result)
-    return list(result)
-
-
-@app.post("/")
-async def add(moto: Moto):
-    lista_moto.append(moto)
-    return lista_moto
-
-
-@app.put("/{name}")
-async def update(name, moto: Moto):
-    print(name)
-    for item in lista_moto:
-        if item.name == name:
-            item = moto
-            return item
-
-    raise HTTPException(status_code=400, detail="NO SE ENCUENTRA EN LA LISTA")
+app.include_router(vehicle.router, prefix="/vehicle")
