fd7addbcf29d1b93f109ee7035b8a46e5398729b
Daniel Ortiz <daniel.ortizvega@mercadolibre.com>
2022-10-24 20:41:58 -0500

    csv

--- scripts/load_csv.py
+++ scripts/load_csv.py
@@ -11,7 +11,7 @@ def create_city(row):
     return response.status_code
 
 
-with open('cities.csv', newline='') as csvfile:
+with open('files/cities.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile, delimiter=',',fieldnames=['description','country','id','name'])
     result = map(lambda row:create_city(row) ,list(spamreader)[1:])
     print(list(result))
