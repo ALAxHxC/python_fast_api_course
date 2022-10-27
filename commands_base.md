5a60ec58e54c7ac71ee2afde4182b828e909e132
Daniel Ortiz <daniel.ortizvega@mercadolibre.com>
2022-10-26 21:09:49 -0500

    Cambios

---
+++ commands_base.md
@@ -0,0 +1,11 @@
+# Hi Laura, have a great day!, 
+#### You forgot any command? remember you can find here
+
+* To set envrioment for sql:
+> export SQL_URI="mysql+mysqldb://user:password@127.0.0.1/db"
+* To start docker
+> docker compose up
+* To stop docker
+> docker compose down
+* to start app:
+> uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, UploadFile, File
+import firebase_admin
+from firebase_admin import credentials, storage
 from controllers import vehicle, city
 from database_sql.connection import Base, engine
 from models.feeling import Moto
@@ -7,17 +8,39 @@ from settings import Settings
 
 app = FastAPI()
 
-settings= Settings()
+settings = Settings()
 
 lista_moto: [Moto] = []
 
 Base.metadata.create_all(bind=engine)
+
+cred = credentials.Certificate("./files/firebase.json")
+firebase_admin.initialize_app(cred)
 
 
 # HOLA LAUUUU
 @app.get("/")
 async def rooto():
     return {"message": settings.mysql_uri}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+        file: UploadFile = File(description="A file read as UploadFile"),
+):
+    read = await file.read()
+
+    PATH =f'public/{file.filename}'
+    f = open(PATH, 'wb')
+    f.write(read)
+    f.close()
+    bucket = storage.bucket(name='roadbook-v1.appspot.com')
+    blob = bucket.blob('test.png')
+    blob.upload_from_filename(PATH)
+    blob.make_public()
+    print("your file url", blob.make_public())
+    return {"filename": blob.public_url}
+
 
 app.include_router(vehicle.router, prefix="/vehicle")
 app.include_router(city.router, prefix="/city")
