65e11088c89a316723d12f05d1c6840e54fe78ed
Daniel Ortiz <daniel.ortizvega@mercadolibre.com>
2022-11-16 19:01:21 -0500

    actualizacion kvs

--- database_sql/queries/brandsqueries.py
+++ database_sql/queries/brandsqueries.py
@@ -6,14 +6,15 @@ from schemas.brands import BrandsSchema
 
 
 class BrandsQueries:
-    def add (self,brands:BrandsSchema,session:Session):
+    def add(self, brands: BrandsSchema, session: Session):
         try:
             session.add(Brands(**brands.dict()))
             session.commit()
-            #AQUI TENEIAMOS ESTE ERROR
-            #return session.query(brands).filter(brands.description==brands.description).first()
+            # AQUI TENIAMOS ESTE ERROR
+            # return session.query(brands).filter(brands.description==brands.description).first()
             return session.query(Brands).filter(Brands.description == brands.description).first()
         except IntegrityError as e:
             raise HTTPException(status_code=400, detail=str(e))
-    def all (sel, session:Session):
+
+    def all(sel, session: Session):
         return session.query(Brands).all()
